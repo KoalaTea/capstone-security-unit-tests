b45e0c5e2f55915c6aea6168233ed923aaa02f20
koalatea <koalateac@gmail.com>
2018-03-07 17:07:20 -0500

    fixed up tests, also added docker

--- passing_apps/flask/tests/hsts_test.py
+++ passing_apps/flask/tests/hsts_test.py
@@ -3,7 +3,7 @@ from exampleapp import create_app
 from bs4 import BeautifulSoup
 
 # LOL THIS PROBABLY WONT WORK :D enabled at web application level
-class CSRFTest(unittest.TestCase):
+class HSTSTest(unittest.TestCase):
     def setUp(self):
         self.app = create_app()
         self.client = self.app.test_client()
@@ -13,13 +13,27 @@ class CSRFTest(unittest.TestCase):
         hsts_headers = [h for h in resp.headers if h[0] == 'Strict-Transport-Security']
         self.assertTrue(hsts_headers)
 
+    def cert_pinning(self, resp):
+        # TODO expand
+        hsts_headers = [h for h in resp.headers if h[0] == 'Public-Key-Pins']
+        self.assertTrue(hsts_headers)
+
     def test_https(self):
         resp = self.client.get('/')
         # test for https being the connection method
         self.assertEquals(resp.status_code, 302)
         self.assertTrue('https' in resp.data.decode('utf-8'))
+
+    def test_hsts(self):
         # test for https working False for testing purposes
         resp = self.client.get('/', base_url='https://localhost')
         self.assertEqual(resp.status_code, 200)
         self.hsts(resp)
+
+    def test_hpkp(self):
         # TODO test for a specific cert
+        # cert_pinning(resp)
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
